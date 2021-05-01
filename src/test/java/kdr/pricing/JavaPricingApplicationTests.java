b85e315a40111475d5b3b78d0cd2ddf322b18748
kennethdroper <kennethdroper@gmail.com>
2021-05-01 23:19:20 +0100

    Improved Spring Test, increase compiler source and target to 8.

--- src/test/java/kdr/pricing/JavaPricingApplicationTests.java
+++ src/test/java/kdr/pricing/JavaPricingApplicationTests.java
@@ -1,11 +1,20 @@
 package kdr.pricing;
 
+import kdr.rest.JavaPricingController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 public class JavaPricingApplicationTests {
+
+    @Autowired
+    private JavaPricingController controller;
+
     @Test
     void contextLoads() {
+        assertThat(controller).isNotNull();
     }
 }
